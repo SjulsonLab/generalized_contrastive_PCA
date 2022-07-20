99fb8fafcbb0e48fe36a7d14500f50ae6a0e479d
eliezyer <fermino.bioeng@gmail.com>
2022-07-20 15:59:17 -0400

    adding python code for ncPCA WSindex version

---
+++ allen_ephys_drifting_gratings_spontaneous_PCs.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 16:54:10 2021
+Script to analyze data from allen institute part of the functional connectivity
+dataset. We want to see if lower rank PCs are able to predict drifting gratings 
+and natural images. The analysis steps will be:
+    
+    1) Get all the sessions from functional_connectivity dataset with VIsp
+    recordings
+    2) Perform PCA during the spontaneous activity recording
+    3) Predict the activity of drifting gratings using windowed PCs and random
+    projection
+
+@author: eliezyer
+"""
+
+#%% importing essentials
+import os
+import shutil
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pynapple as nap
+
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from matplotlib.pyplot import *
+from scipy import stats
+
+#%% parameters
+min_n_cell = 50
+
+
+#%% preparing data to be loaded
+
+data_directory = '/mnt/probox/allen_institute_data/ecephys/' # must be a valid directory in your filesystem
+manifest_path = os.path.join(data_directory, "manifest.json")
+cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+
+# getting only the sessions from functonal connectivity dataset and that contain cells from VISp
+sessions = cache.get_session_table()
+#selected_sessions = sessions[(sessions.session_type == 'functional_connectivity') & \
+#                            (['VISp' in acronyms for acronyms in
+#                               sessions.ecephys_structure_acronyms])]
+    
+    
+selected_sessions = sessions[(sessions.session_type == 'brain_observatory_1.1')]
+    
+#%% getting how many VISp units there are there
+#number_of_neurons = np.zeros(selected_sessions.shape[0]);
+#c = 0;
+#for session_id, row in selected_sessions.iterrows():
+#    
+#    loaded_session = cache.get_session_data(session_id)
+#    number_of_neurons[c] = loaded_session.structurewise_unit_counts.VISp
+#    c = c+1;
+#    print('Session ' + str(session_id) + ' completed. Found ' + str(number_of_neurons[c-1]) + ' VISp cells')
+    
+    
+#%% performing analysis, session loop starts here
+
+#ses2use = np.where(number_of_neurons==np.max(number_of_neurons));
+#ses2use = number_of_neurons==np.max(number_of_neurons);
+sessions2load = selected_sessions.index.values
+
+"this for is for later, once I have the rest of the loop analysis figured out"
+#for a in np.arange(0,len(sessions2load)):
+#   session_id = sessions2load[a]
+session_id = 715093703;
+loaded_session = cache.get_session_data(session_id)
+
+#getting natural scenes and static gratings info
+stimuli_info = loaded_session.get_stimulus_table(["natural_scenes","static_gratings"])
+
+#getting spikes times and information
+temp_spk_ts = loaded_session.spike_times
+temp_spk_info  = loaded_session.units
+
+#here I'm picking all the brain areas that are related to visual system, i.e.,
+#starting with V or LG
+spikes_info = temp_spk_info[temp_spk_info["ecephys_structure_acronym"].str.contains("V|LG")]
+units2use = spikes_info.index.values
+
+temp_spk_ts_2 = {}
+for aa in np.arange(0,len(units2use)):
+    temp_spk_ts_2[aa] = temp_spk_ts[units2use[aa]]
+
+#passing to pynapple ts group
+spikes_times = nap.TsGroup(temp_spk_ts_2)
+
+#adding structure info into the spike times TSgroup 
+structs = pd.DataFrame(index=np.arange(len(units2use)),data = spikes_info.ecephys_structure_acronym.values,columns=['struct'])
+spikes_times.set_info(structs)
+
+#passing natural scenes to intervalsets
+df_stim_ns = stimuli_info.query("stimulus_name=='natural_scenes'");
+ns_intervals = nap.IntervalSet(start=df_stim_ns.start_time.values,end=df_stim_ns.stop_time.values)
+
+df_stim_sg = stimuli_info.query("stimulus_name=='static_gratings'");
+sg_intervals = nap.IntervalSet(start=df_stim_sg.start_time.values,end=df_stim_sg.stop_time.values)
+
+#binning through using a loop in the cells until I can find a better way
+spikes_binned_ns = np.empty((len(ns_intervals.values),len(spikes_times.data)))
+bins_ns = ns_intervals.values.flatten()
+for aa in np.arange(len(spikes_times.data)):
+    tmp = np.array(np.histogram(spikes_times.data[aa].index.values,bins_ns))
+    spikes_binned_ns[:,aa] = tmp[0][np.arange(0,tmp[0].shape[0],2)]
+
+#same method of binning through a loop in the cells, but for static gratings
+spikes_binned_sg = np.empty((len(sg_intervals.values),len(spikes_times.data)))
+bins_sg = sg_intervals.values.flatten()
+for aa in np.arange(len(spikes_times.data)):
+    tmp = np.array(np.histogram(spikes_times.data[aa].index.values,bins_sg))
+    spikes_binned_sg[:,aa] = tmp[0][np.arange(0,tmp[0].shape[0],2)]
+    
+#getting labels
+sg_labels = df_stim_sg.stimulus_condition_id.values
+ns_labels = df_stim_ns.stimulus_condition_id.values
+#%% performing prediction
+
+array_of_ba = spikes_info["ecephys_structure_acronym"].unique();
+scores_ns = np.empty((5,len(array_of_ba)))
+scores_sg = np.empty((5,len(array_of_ba)))
+
+spikes_zsc_ns = stats.zscore(spikes_binned_ns)
+spikes_zsc_sg = stats.zscore(spikes_binned_sg)
+'''for aa in np.arange(len(array_of_ba)):
+    clf_ns = svm.SVC()
+    clf_sg = svm.SVC()
+    units_idx = spikes_info["ecephys_structure_acronym"]==array_of_ba[aa]
+    scores_ns[:,aa] = cross_val_score(clf_ns,spikes_zsc_ns[:,units_idx],ns_labels,cv=5)
+    scores_sg[:,aa] = cross_val_score(clf_sg,spikes_zsc_sg[:,units_idx],sg_labels,cv=5)
+'''
+#%% getting ncPCA loadings
+brain_area_dict = {};
+for aa in np.arange(len(array_of_ba)):
+    units_idx = spikes_info["ecephys_structure_acronym"]==array_of_ba[aa]
+    if sum(units_idx.values) >= min_n_cell:
+        X,S = ncPCA(spikes_binned_sg[:,units_idx],spikes_binned_ns[:,units_idx])
+        _,_,Vns = np.linalg.svd(spikes_zsc_ns[:,units_idx],full_matrices=False)  
+        scores_vns = np.empty((5,len(Vns)))
+        scores_x = np.empty((5,len(Vns)))
+        for aaa in np.arange(len(Vns)):
+            clf_vns = svm.SVC()
+            clf_x = svm.SVC()
+            scores_x[:,aaa] = cross_val_score(clf_x,np.dot(spikes_zsc_ns[:,units_idx],X[:,:aaa+1]),ns_labels,cv=5)
+            scores_vns[:,aaa] = cross_val_score(clf_vns,np.dot(spikes_zsc_ns[:,units_idx],Vns[:aaa+1,:].T),ns_labels,cv=5)
+        brain_area_dict['scores_x_'+array_of_ba[aa]] = scores_x
+        brain_area_dict['scores_vns_'+array_of_ba[aa]] = scores_vns
+#%% testing prediction over X and over Vns
+
+
+
+#%% getting stimulus epoch, stimulus identity and spikes so we can conduct analysis
+
+#this gets the stimulus table specifics test = loaded_session.get_stimulus_table("natural_movie_one_more_repeats")
+#this gets the start and stop times. test.start_time test.stop_time
+#this gets the spikes spike_times = loaded_session.spike_times
+#this gets unit information unit_info=loaded_session.units
+#this gets the brain area acronym unit_info["ecephys_structure_acronym"]
---
+++ ncPCA.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  7 09:57:15 2022
+
+@author: fermi
+script to implement ncPCA and run some tests on it
+"""
+
+
+#%% defining important functions
+
+#fast reduced row echelon form function
+def frref(A, TOL=None, TYPE=''):
+    '''
+    %FRREF   Fast reduced row echelon form.
+    %   R = FRREF(A) produces the reduced row echelon form of A.
+    %   [R,jb] = FRREF(A,TOL) uses the given tolerance in the rank tests.
+    %   [R,jb] = FRREF(A,TOL,TYPE) forces frref calculation using the algorithm
+    %   for full (TYPE='f') or sparse (TYPE='s') matrices.
+    %
+    %
+    %   Description:
+    %   For full matrices, the algorithm is based on the vectorization of MATLAB's
+    %   RREF function. A typical speed-up range is about 2-4 times of
+    %   the MATLAB's RREF function. However, the actual speed-up depends on the
+    %   size of A. The speed-up is quite considerable if the number of columns in
+    %   A is considerably larger than the number of its rows or when A is not dense.
+    %
+    %   For sparse matrices, the algorithm ignores the TOL value and uses sparse
+    %   QR to compute the rref form, improving the speed by a few orders of
+    %   magnitude.
+    %
+    %   Authors: Armin Ataei-Esfahani (2008)
+    %            Ashish Myles (2012)
+    #            Snehesh Shrestha (2020)
+    %
+    %   Revisions:
+    %   25-Sep-2008   Created Function
+    %   21-Nov-2012   Added faster algorithm for sparse matrices
+    #   30-June-2020  Ported to python. TODO: Only do_full implemented. The remaining of the function. See frref_orig below.
+    '''
+    
+    import numpy as np
+    from scipy.sparse import isspmatrix  # ,csr_matrix
+
+    m = np.shape(A)[0]
+    n = np.shape(A)[1]
+
+    # Process Arguments
+    # ----------------------------------------------------------
+    # TYPE -- Sparce (s) or non-Sparce (Full, f)
+    if TYPE == '':   # set TYPE if sparse or not
+        if isspmatrix(A):
+            TYPE = 's'
+        else:
+            TYPE = 'f'
+    else:   # Set type
+        if not type(TYPE) is str or len(TYPE) > 1:  # Check valid type
+            print('Unknown matrix TYPE! Use "f" for full and "s" for sparse matrices.')
+            exit()
+
+        TYPE = TYPE.lower()
+        if not TYPE == 'f' and not TYPE == 's':
+            print(
+                'Unknown matrix TYPE! Use ''f'' for full and ''s'' for sparse matrices.')
+            exit()
+
+    if TYPE=='f':
+        # TOLERENCE
+        # % Compute the default tolerance if none was provided.
+        if TOL is None:
+            # Prior commit had TOL default to 1e-6
+            # TOL = max(m,n)*eps(class(A))*norm(A,'inf')
+            TOL = max(m, n)*np.spacing(type(A)(1))*np.linalg.norm(A, np.inf)
+
+    # Non-Sparse
+    # ----------------------------------------------------------
+    if not isspmatrix(A) or TYPE == 'f':
+        # % Loop over the entire matrix.
+        i = 0
+        j = 0
+        jb = []
+
+        while (i < m) and (j < n):
+            # % Find value (p) and index (k) of largest element in the remainder of column j.
+            abscol = np.array(np.abs(A[i:m, j]))
+            p = np.max(abscol)
+            k = np.argmax(abscol, axis=0)
+            if np.ndim(k) > 1:
+                k = k[0]
+            else:
+                k = int(k)
+
+            k = k+i  # -1 #python zero index, not needed
+
+            if p <= TOL:
+                # % The column is negligible, zero it out.
+                A[i:m, j] = 0  # %(faster for sparse) %zeros(m-i+1,1);
+                j += 1
+            else:
+                # % Remember column index
+                jb.append(j)
+
+                # % Swap i-th and k-th rows.
+                A = np.array(A)
+                A[[i, k], j:n] = A[[k, i], j:n]
+
+                # % Divide the pivot row by the pivot element.
+                Ai = np.nan_to_num(A[i, j:n] / A[i, j])
+                Ai = np.matrix(Ai).T.T
+
+                # % Subtract multiples of the pivot row from all the other rows.
+                A[:, j:n] = A[:, j:n] - np.dot(A[:, [j]], Ai)
+                A[i, j:n] = Ai
+                i += 1
+                j += 1
+
+        return A, jb
+
+    # Sparse
+    # ----------------------------------------------------------
+    else:
+        A = np.array(A.toarray())
+        return frref(A, TYPE='f')
+
+        # # TODO: QR-decomposition of a Sparse matrix is not so simple in Python -- still need to figure out a solution
+        # # % Non-pivoted Q-less QR decomposition computed by Matlab actually
+        # # % produces the right structure (similar to rref) to identify independent
+        # # % columns.
+        # R = numpy.linalg.qr(A)
+
+        # # % i_dep = pivot columns = dependent variables
+        # # %       = left-most non-zero column (if any) in each row
+        # # % indep_rows (binary vector) = non-zero rows of R
+        # [indep_rows, i_dep] = np.max(R ~= 0, [], 2)     # TODO
+        # indep_rows = full[indep_rows]; # % probably more efficient
+        # i_dep = i_dep[indep_rows]
+        # i_indep = setdiff[1:n, i_dep]
+
+        # # % solve R(indep_rows, i_dep) x = R(indep_rows, i_indep)
+        # # %   to eliminate all the i_dep columns
+        # # %   (i.e. we want F(indep_rows, i_dep) = Identity)
+        # F = sparse([],[],[], m, n)
+        # F[indep_rows, i_indep] = R[indep_rows, i_dep] \ R[indep_rows, i_indep]
+        # F[indep_rows, i_dep] = speye(length(i_dep))
+
+        # # % result
+        # A = F
+        # jb = i_dep
+
+        # return A, jb
+
+def ncPCA(N1,N2,Nshuffle=0):
+    """function [X,S_total] = ncPCA(Ns, Nw, Nshuffle)
+    %
+    % This function does normalized contrastive PCA (nvPCA) on binned spike trains to
+    % get the WS index (W-S)/(W+S) <--- this extracts dimensions that are
+    % maximally present in wakefulness but not sleep.
+    %
+    % INPUTS
+    % Ns        -- (t1 x n) matrix of binned spike trains during sleep
+    % Nw        -- (t2 x n) matrix of binned spike trains during awake behavior
+    % Nshuffle  -- (optional) number of times to shuffle for null distribution
+    %
+    % OUTPUTS
+    % B        -- maxSPV scores (temporal eigenvectors)
+    % S        -- amount of variance captured for sleep, awake, sleep_shuf, awake_shuf
+    % X        -- maxSPV loadings for both awake and asleep data
+    % info     -- extra info
+    %
+    % Note: Ns and Nw should both be normalized (as in normalize(zscore(Ns), 'norm'))
+    %
+    % Analogous to SVD, which decomposes data matrix N into (U * S * T'), this
+    % function decomposes N into (B * S * X') where B is the temporal
+    % eigenvectors (or PC scores), S is a diagonal matrix representing amount
+    % of variance captured, and X is the PC loadings across cells. Instead of
+    % maximizing var(N) in each dimension, this ncPCA finds the spPCs X that maximize
+    %
+    %            (x' * (Nw'*Nw - Ns'*Ns) * x) / (x' * (Nw'*Nw + Ns'*Ns) * x)
+    %
+    % where Ns is asleep data and Nw is awake data. Because Ns and Nw may not
+    % be full rank, X is calculated in a subspace spanned by PCs accounting for
+    % 99.5% of the variance.
+    %
+    % Luke Sjulson, 2021-11-04 
+    """
+    
+    #importing libraries
+    import numpy as np
+    from scipy import stats
+    from scipy import linalg as LA
+    import warnings
+
+    #parameters
+    cutoff = 0.995 #keeping this much variance with PCA
+    
+    #test that inputs are normalized
+    if N2.shape[1] != N1.shape[1]:
+        raise ValueError("N1 and N2 have different numbers of features")
+    
+    N1_temp = np.divide(stats.zscore(N1),np.linalg.norm(stats.zscore(N1),axis=0))
+    N2_temp = np.divide(stats.zscore(N2),np.linalg.norm(stats.zscore(N2),axis=0))
+    
+    if np.sum(np.sum(np.square(N1_temp - N1)) > (0.01*np.square(N1_temp))):
+        warnings.warn("N1 was not normalized properly - normalizing now")
+        N1 = N1_temp
+    
+    if np.sum(np.sum(np.square(N2_temp - N2)) > (0.01*np.square(N2_temp))):
+        warnings.warn("N2 was not normalized properly - normalizing now")
+        N2 = N2_temp
+    
+    #SVD (or PCA) on N1 and N2
+    _,S1,V1 = np.linalg.svd(N1,full_matrices = False)
+    _,S2,V2 = np.linalg.svd(N2,full_matrices = False)
+    
+    # discard PCs that cumulatively account for less than 1% of variance, i.e.
+    # rank-deficient dimensions
+    S1_diagonal = S1
+    S2_diagonal = S2
+    
+    #cumulative variance
+    cumvar_1 = np.divide(np.cumsum(S1_diagonal),np.sum(S1_diagonal))
+    cumvar_2 = np.divide(np.cumsum(S2_diagonal),np.sum(S2_diagonal))
+    
+    #picking how many PCs to keep
+    max_1 = np.where(cumvar_1 < cutoff)
+    max_2 = np.where(cumvar_2 < cutoff)
+    
+    # Zassenhaus algorithm to find shared basis for N1 and N2
+    # https://en.wikipedia.org/wiki/Zassenhaus_algorithm
+    V1_hat = V1[max_1[0],:];
+    V2_hat = V2[max_2[0],:];
+    
+    N_dim = V1.shape[1]
+    V1_cat = np.concatenate((V1_hat,V1_hat),axis=1)
+    V2_cat = np.concatenate((V2_hat,np.zeros(V2_hat.shape)),axis=1)
+    zassmat = np.concatenate((V1_cat,V2_cat),axis=0)
+    zassmat_rref,_ = frref(zassmat)
+    
+    basis_idx = 0
+    basis_mat = [];
+    for idx in np.arange(0,np.shape(zassmat_rref)[0]):
+        if np.all(zassmat_rref[idx,:N_dim] == 0): #this is a basis vector
+            #basis_mat = np.concatenate((basis_mat,zassmat_rref[idx,N_dim:].T),axis=1)
+            basis_mat.append(zassmat_rref[idx,N_dim:].T)
+            basis_idx += 1
+    
+    basis_mat2 = np.array(basis_mat).T
+    
+    J = LA.orth(basis_mat2) #orthonormal shared basis for Ns and Nw
+    k = J.shape[1]
+    
+    ## Calculating ncPCA below
+    
+    #covariance matrices
+    N1N1 = np.dot(N1.T,N1)
+    N2N2 = np.dot(N2.T,N2)
+    
+    B = LA.sqrtm(np.linalg.multi_dot((J.T,N2N2+N1N1,J)))
+    
+    #JBinv = np.linalg.lstsq(np.linalg.inv(J),np.linalg.inv(B))
+    #JBinv,_,_,_ = np.linalg.lstsq(J.T,np.linalg.inv(B))
+    #JBinv = np.linalg.solve(J.T,B.T)
+    #JBinv = np.dot(J,np.linalg.pinv(B))
+    
+    JBinv =  np.linalg.lstsq(J.T, np.linalg.pinv(B))[0]
+    
+    # sort according to decreasing eigenvalue
+    D,y = np.linalg.eig(np.linalg.multi_dot((JBinv.T,N2N2-N1N1,JBinv)))
+    
+    Y = y[:,np.flip(np.argsort(D))]
+    
+    X_temp = np.dot(JBinv,Y);
+    X = X_temp/np.linalg.norm(X_temp,axis=0)
+    
+    # getting top and bottom eigenvalues
+    Stop = np.linalg.multi_dot((X.T,N2N2-N1N1,X))
+    Sbot = np.linalg.multi_dot((X.T,N2N2+N1N1,X))
+    S_total = np.divide(np.diagonal(Stop),np.diagonal(Sbot))
+    
+    
+    #write shuffling later
+    
+    return X,S_total
